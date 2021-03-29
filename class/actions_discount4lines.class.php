4e9f36ef76e913bb08474eb7291eb0aeca0434da
kevin <kevin.giuga@atm-consulting.fr>
2021-03-29 11:44:29 +0200

    Force -HT- product_base_type to avoid modification of P.U HT after discount applied

--- class/actions_discount4lines.class.php
+++ class/actions_discount4lines.class.php
@@ -44,7 +44,7 @@ class ActionsDiscount4lines
 	function formConfirm($parameters, &$object, &$action, $hookmanager)
 	{
 		global $langs,$db,$user, $conf;
-		
+
 		$error = 0;
 
 		$langs->load('discount4lines@discount4lines');
@@ -52,16 +52,16 @@ class ActionsDiscount4lines
 		$contexts = explode(':',$parameters['context']);
 
 		if(in_array('propalcard',$contexts) || in_array('invoicecard',$contexts) || in_array('ordercard',$contexts) || in_array('ordersuppliercard', $contexts) || in_array('invoicesuppliercard', $contexts)) {
-			
+
 			$userHasRights = ! empty($user->rights->{$object->element}->creer);
 			if ($object->element == 'order_supplier') $userHasRights = ! empty($user->rights->fournisseur->commande->creer);
 			if ($object->element == 'invoice_supplier') $userHasRights = ! empty($user->rights->fournisseur->facture->creer);
-			
+
 			if ($object->statut == 0  && $userHasRights) {
-				
+
 				if($action == 'ask_discount4lines') {
 					$form = new Form($this->db);
-					
+
 					$actionform = 'discount4lines';
 					$title = $langs->trans('ApplyDiscount4linesTitle');
 					$question = '';
@@ -71,12 +71,12 @@ class ActionsDiscount4lines
 					$selectedchoice = 'yes';
 					$useajax = 1;
 					$out = $form->formconfirm($_SERVER['PHP_SELF'].'?ref='.$object->ref, $title, $question, $actionform, $formquestion, $selectedchoice, $useajax);
-					
+
 					if (! $error)
 					{
 						$this->results = array();
 						$this->resprints = $out;
-						
+
 						return 0; // or return 1 to replace standard code
 					}
 					else
@@ -85,13 +85,13 @@ class ActionsDiscount4lines
 						return -1;
 					}
 				}
-			
-				
-				
+
+
+
 			}
 		}
 	}
-	
+
 	/** Overloading the doActions function : replacing the parent's function with the one below
 	 * @param      $parameters  array           meta datas of the hook (context, etc...)
 	 * @param      $object      CommonObject    the object you want to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
@@ -102,9 +102,9 @@ class ActionsDiscount4lines
 	function doActions($parameters, &$object, &$action, $hookmanager)
 	{
 		global $langs,$db,$user, $conf;
-	
+
 		$langs->load('discount4lines@discount4lines');
-	
+
 		$contexts = explode(':',$parameters['context']);
 		if( in_array('propalcard',$contexts) || in_array('invoicecard',$contexts) || in_array('ordercard', $contexts) || in_array('ordersuppliercard', $contexts) || in_array('invoicesuppliercard', $contexts)) {
 
@@ -114,10 +114,10 @@ class ActionsDiscount4lines
 
 			if ($object->statut == 0  && $userHasRights) {
 				if($action == 'discount4lines') {
-					
+
 					$countLineUpdated = 0;
 					$err = 0;
-					
+
 					foreach($object->lines as $line) {
 						if(
 								($line->product_type == '0' && ! empty($conf->global->DISCOUNT4LINES_APPLY_TO_PRODUCTS))	// Si produit et qu'on applique la réduc sur les produits
@@ -126,9 +126,9 @@ class ActionsDiscount4lines
 						{
 							$remise_percent = GETPOST('amount_discount4lines','int');
 							if($line->total_ht > 0) {
-								
+
 								if(in_array('propalcard',$contexts)) {
-								
+
 									$res = $object->updateline(
 										$line->id,
 									isset($line->subprice) ? $line->subprice : $line->price,
@@ -138,7 +138,7 @@ class ActionsDiscount4lines
 										$line->localtax1_tx,
 										$line->localtax2_tx,
 										$line->desc,
-										$line->price_base_type,
+										'HT',
 										$line->infobits,
 										$line->special_code,
 										$line->fk_parent_line,
@@ -155,25 +155,25 @@ class ActionsDiscount4lines
 									);
 								} elseif(in_array('invoicecard',$contexts)) {
 									$res = $object->updateline(
-										$line->id, 
-										$line->desc, 
+										$line->id,
+										$line->desc,
 									isset($line->subprice) ? $line->subprice : $line->price,
-										$line->qty, 
-										$remise_percent, 
-										$line->date_start, 
-										$line->date_end, 
-										$line->tva_tx, 
-										$line->localtax1_tx, 
-										$line->localtax2_tx, 
-										$line->price_base_type, 
-										$line->infobits, 
+										$line->qty,
+										$remise_percent,
+										$line->date_start,
+										$line->date_end,
+										$line->tva_tx,
+										$line->localtax1_tx,
+										$line->localtax2_tx,
+										'HT',
+										$line->infobits,
 										$line->product_type, // type
-										$line->fk_parent_line, 
-										$line->skip_update_total, 
-										$line->fk_fournprice, 
-										$line->pa_ht=0, 
-										$line->label, 
-										$line->special_code, 
+										$line->fk_parent_line,
+										$line->skip_update_total,
+										$line->fk_fournprice,
+										$line->pa_ht=0,
+										$line->label,
+										$line->special_code,
 										$line->array_options,
 										$line->situation_percent,
 										$line->fk_unit,
@@ -256,7 +256,7 @@ class ActionsDiscount4lines
 					if($countLineUpdated > 0) {
 						setEventMessage($langs->trans('Discount4linesApplied', $countLineUpdated));
 					}
-					
+
 					// Si commande fournisseur, il n'y a pas de redirection, donc l'affichage des lignes n'est pas mis à jour : on redirige
 					if(in_array('ordersuppliercard', $contexts)) {
 						header("Location: ".$_SERVER['PHP_SELF']."?id=".$object->id);
@@ -266,31 +266,31 @@ class ActionsDiscount4lines
 			}
 		}
 	}
-	
-	
-			
+
+
+
 
 	function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager) {
-		
+
 		global $langs,$db,$user, $conf;
-		
+
 		$contexts = explode(':',$parameters['context']);
-		
+
 		if( in_array('propalcard',$contexts) || in_array('invoicecard',$contexts) || in_array('ordercard',$contexts) || in_array('ordersuppliercard', $contexts) || in_array('invoicesuppliercard', $contexts)) {
 
 			$userHasRights = ! empty($user->rights->{$object->element}->creer);
 			if ($object->element == 'order_supplier') $userHasRights = ! empty($user->rights->fournisseur->commande->creer);
 			if ($object->element == 'invoice_supplier') $userHasRights = ! empty($user->rights->fournisseur->facture->creer);
-				
+
 			if ($object->statut == 0  && $userHasRights) {
-		
+
 				$out = '<div class="inline-block divButAction"><a class="butAction" href="' . $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&amp;action=ask_discount4lines">' . $langs->trans('BtnDiscount4Lines') . '</a></div>';
-			
+
 				$this->results = array();
 				$this->resprints = $out;
 				print $out;
 				return 0; // or return 1 to replace standard code
 			}
-		}	
+		}
 	}
 }
